9f7e89ce5c36092c4e81c81ed3dc289c23b51663
Oleksandr Shevchyk <shevchuk-ole@i.ua>
2017-11-30 14:58:06 +0200

    fix bag && continue code

--- src/script/main.js
+++ src/script/main.js
@@ -1,12 +1,25 @@
 var $addUser =  jQuery("#AllUser");
 var $selectedUser = jQuery("#SelectedUser");
+var $lead = jQuery("#teamLead");
 
 $(".btnclick").click(function() {
+
+    if(document.getElementById("SelectedUser").value==""){
+        toastr.error("Please select a user");
+    } else{
+        $lead.append("<option>"+$selectedUser.val()+"</option>");
+        $lead.val(jQuery("#teamLead>option").last().text());
+        toastr.success("Team Lead are selected");
+    }
+});
+jQuery(".refresh").click(function(el){
+    el.preventDefault();
     $selectedUser.append("<option>"+$addUser.val()+"</option>");
-    jQuery("#SelectedUser").val(jQuery("#SelectedUser>option").last().text())
-    toastr.success("User are selected")
+    jQuery("#SelectedUser").val(jQuery("#SelectedUser>option").last().text());
+    toastr.success("User are selected");
 });
 jQuery(document).ready(function(){
+    jQuery(".all-user").val("Stepan");
     $( function() {
         $( "#tabs" ).tabs();
     } );
